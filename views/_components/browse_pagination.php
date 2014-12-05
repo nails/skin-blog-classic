59c6194bb0fb5dbc4ddc93016fc8b11eae71aa2d
Pablo de la Peña <p@shedcollective.org>
2014-12-05 11:35:53 +0000

    Pagination fixes

--- views/_components/browse_pagination.php
+++ views/_components/browse_pagination.php
@@ -1,17 +1,36 @@
 <?php
 
-	$this->load->library('pagination');
+    $this->load->library('pagination');
 
-	$_config						= array();
-	$_config['base_url']			= site_url( app_setting( 'url', 'blog-' . $blog_id ) );
-	$_config['total_rows']			= $pagination->total;
-	$_config['per_page']			= $pagination->per_page;
-	$_config['use_page_numbers']	= TRUE;
-	$_config['use_rsegment']		= TRUE;
-	$_config['uri_segment']			= 2;
-	$_config['full_tag_open']		= '<li class="pagination">';
-	$_config['full_tag_close']		= '</li>';
+    $config                     = array();
+    $config['base_url']         = $blog_url;
+    $config['total_rows']       = $pagination->total;
+    $config['per_page']         = $pagination->per_page;
+    $config['use_page_numbers'] = true;
+    $config['use_rsegment']     = true;
+    $config['uri_segment']      = 3;
 
-	$this->pagination->initialize( $_config );
+    $config['full_tag_open']    = '<li class="text-center"><ul class="pagination">';
+    $config['full_tag_close']   = '</ul></li>';
+    $config['num_tag_open']     = '<li>';
+    $config['num_tag_close']    = '</li>';
+    $config['cur_tag_open']     = '<li class="disabled"><li class="active"><a href="#">';
+    $config['cur_tag_close']    = '<span class="sr-only"></span></a></li>';
+    $config['next_tag_open']    = '<li>';
+    $config['next_link']        = '&rsaquo;';
+    $config['next_tagl_close']  = '</li>';
+    $config['prev_tag_open']    = '<li>';
+    $config['prev_link']        = '&lsaquo;';
+    $config['prev_tagl_close']  = '</li>';
+    $config['first_tag_open']   = '<li>';
+    $config['first_link']       = '&laquo; First';
+    $config['first_tagl_close'] = '</li>';
+    $config['last_tag_open']    = '<li>';
+    $config['last_link']        = 'Last &raquo;';
+    $config['last_tagl_close']  = '</li>';
 
-	echo '<li>' . $this->pagination->create_links() . '</li>';
+    $this->pagination->initialize($config);
+
+    echo '<li>';
+        echo $this->pagination->create_links();
+    echo '</li>';
