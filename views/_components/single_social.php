f97387ad10faf3cb9e67e162ee1cd35e0ea781fc
Pablo de la Peña <p@shedcollective.org>
2015-05-15 16:01:10 +0100

    Typo in social share

--- views/_components/single_social.php
+++ views/_components/single_social.php
@@ -3,8 +3,8 @@
     //  Defaults
     $layout      = '';
     $singleTitle = app_setting('social_layout_single_text', 'blog-' . $blog->id) ? app_setting('social_layout_single_text', 'blog-' . $blog->id) : 'Share';
-    $counters    = app_setting('socialcounters', 'blog-' . $blog->id) ? 'data-zeroes="yes"' : 'data-counters="no"';
-    $twitterVia  = app_setting('socialtwitterVia', 'blog-' . $blog->id) ? app_setting('socialtwitterVia', 'blog-' . $blog->id) : '';
+    $counters    = app_setting('social_counters', 'blog-' . $blog->id) ? 'data-zeroes="yes"' : 'data-counters="no"';
+    $twitterVia  = app_setting('social_twitter_via', 'blog-' . $blog->id) ? 'data-via="' . app_setting('social_twitter_via', 'blog-' . $blog->id) . '"' : '';
 
     // --------------------------------------------------------------------------
 
@@ -26,7 +26,7 @@
 
     $enabled   = array();
     $enabled[] = app_setting('social_facebook_enabled', 'blog-' . $blog->id) ? '<div class="facebook" title="Share link on Facebook">Facebook</div>' : '';
-    $enabled[] = app_setting('social_twitter_enabled', 'blog-' . $blog->id) ? '<div class="twitter" data-via="' . $twitterVia . '" title="Share link on Twitter">Twitter</div>' : '';
+    $enabled[] = app_setting('social_twitter_enabled', 'blog-' . $blog->id) ? '<div class="twitter" ' . $twitterVia . ' title="Share link on Twitter">Twitter</div>' : '';
     $enabled[] = app_setting('social_googleplus_enabled', 'blog-' . $blog->id) ? '<div class="plusone" title="Share link on Google+">Google+</div>' : '';
     $enabled[] = app_setting('social_pinterest_enabled', 'blog-' . $blog->id) && $post->image_id ? '<div class="pinterest" data-media="' . cdn_serve($post->image_id) . '" title="Share image on Pinterest">Pinterest</div>' : '';
 
