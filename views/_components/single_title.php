3b24a9942fd71f163544569557515cbb5d900d86
Pablo de la Pena <p@shedcollective.org>
2015-07-25 22:26:07 +0100

    Small tweaks
    - Colours
    - Added alert class to "not yet published" message

--- views/_components/single_title.php
+++ views/_components/single_title.php
@@ -1,16 +1,17 @@
 <?php
 
-    echo '<h2 class="title">' . $post->title . '</h2>';
+echo '<h2 class="title">' . $post->title . '</h2>';
+
+if ($post->is_published) {
+
     echo '<p class="date-author">';
-
-    if ($post->is_published) {
-
         echo 'Published ' . toUserDatetime($post->published). ', ';
         echo 'by ' . $post->author->first_name . ' ' . $post->author->last_name;
-
-    } else {
-
-        echo 'This post has not yet been published.';
-    }
-
     echo '</p>';
+
+} else {
+
+    echo '<p class="not-published alert alert-warning">';
+        echo 'This post has not yet been published.';
+    echo '</p>';
+}
